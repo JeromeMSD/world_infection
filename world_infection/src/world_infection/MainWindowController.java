c12d1509551b747d1783e94489f5a6c9a0f056c2
jeromem <jeromem@jeromem-N551JX>
2018-03-12 22:42:04 +0100

    First commit

---
+++ world_infection/src/world_infection/MainWindowController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package world_infection;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author jeromem
+ */
+public class MainWindowController implements Initializable {
+
+    
+    @FXML
+    public void reset(){
+        
+    }
+    
+    @FXML
+    public void play(){
+        
+    }
+    
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ world_infection/src/world_infection/World_infection.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package world_infection;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+/**
+ *
+ * @author jeromem
+ */
+public class World_infection extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fl = new FXMLLoader(getClass().getResource("/fxml/MainWindow.fxml"));
+        fl.setController(new MainWindowController());
+        Parent root = fl.load();
+                
+        Scene scene = new Scene(root, 800, 700);
+
+        stage.setTitle("World Infection");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
